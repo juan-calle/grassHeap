f17cce7f060f43356da6f4ff58ae6574d9f08dce
Gil Reich <gilreich12@gmail.com>
2021-06-16 15:19:13 +0100

    server database config for deployment

--- server/models/database.js
+++ server/models/database.js
@@ -1,7 +1,13 @@
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/grassHeap', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
+const DB_USER = process.env.DB_USER;
+const DB_USER_PASSWORD = process.env.DB_USER_PASSWORD;
+
+const mongoose = require("mongoose");
+mongoose.connect(
+  `mongodb+srv://${DB_USER}:${DB_USER_PASSWORD}@cluster0.kbmrd.mongodb.net/grassHeap?retryWrites=true&w=majority`,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
 
 const db = mongoose.connection;
