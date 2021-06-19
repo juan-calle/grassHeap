a2c832cf642e4950d2fe0b7e4ffaa766de01aa9f
Gil Reich <gilreich12@gmail.com>
2021-06-19 18:54:22 +0100

    feat: made easier to run server locally

--- server/models/database.js
+++ server/models/database.js
@@ -1,13 +1,16 @@
+require("dotenv").config();
 const DB_USER = process.env.DB_USER;
 const DB_USER_PASSWORD = process.env.DB_USER_PASSWORD;
 
+let connectionURI;
+if (DB_USER && DB_USER_PASSWORD)
+  connectionURI = `mongodb+srv://${DB_USER}:${DB_USER_PASSWORD}@cluster0.kbmrd.mongodb.net/grassHeap?retryWrites=true&w=majority`;
+else connectionURI = `mongodb://localhost:27017/grassHeap`;
+
 const mongoose = require("mongoose");
-mongoose.connect(
-  `mongodb+srv://${DB_USER}:${DB_USER_PASSWORD}@cluster0.kbmrd.mongodb.net/grassHeap?retryWrites=true&w=majority`,
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  }
-);
+mongoose.connect(connectionURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 const db = mongoose.connection;
