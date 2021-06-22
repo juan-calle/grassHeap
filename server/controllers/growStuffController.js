d578fe6f27df81c1a225e7ef7bcce79bfc9f3f31
Calimeron <ron.chamma@gmail.com>
2021-06-22 20:36:21 +0200

    :recycle:  refactor growstuffcontroller

--- server/controllers/growStuffController.js
+++ server/controllers/growStuffController.js
@@ -2,20 +2,24 @@ const fetch = require("node-fetch");
 
 const base_url = "https://www.growstuff.org/crops";
 
+async function augmentPlants (plants) {
+  const plantsAugmented = await Promise.all(
+    plants.map(async (plant) => {
+      const JSONplant = await fetch(`${base_url}/${plant.slug}.json`);
+      const plantDetails = await JSONplant.json();
+      const details = plantDetails.openfarm_data;
+      plant.details = details;
+      return plant;
+    })
+  );
+  return plantsAugmented;
+}
+
 async function getAllPlants(_, res) {
   try {
     const JSONplants = await fetch(`${base_url}.json`);
     const plants = await JSONplants.json();
-    const plantsAugmented = await Promise.all(
-      plants.map(async (plant) => {
-        const JSONplant = await fetch(`${base_url}/${plant.slug}.json`);
-        const plantDetails = await JSONplant.json();
-        const details = plantDetails.openfarm_data;
-        plant.details = details;
-        return plant;
-      })
-    );
-    console.log("plants augmented", plantsAugmented[0]);
+    const plantsAugmented = await augmentPlants(plants);
     res.status(200).send(plantsAugmented);
   } catch (err) {
     res.status(400).send("failed to get all Plants", err);
