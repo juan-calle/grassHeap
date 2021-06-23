f92bfe3a4312acdaacff3469a7cdb9830ad9270a
Calimeron <ron.chamma@gmail.com>
2021-06-23 16:50:42 +0200

    :clown_face:  added customTasks to mocks

--- server/controllers/__tests__/mocks.js
+++ server/controllers/__tests__/mocks.js
@@ -1,10 +1,24 @@
 module.exports = {
   myPlants1: {
-    name: 'pumpkin',
+    name: "pumpkin",
     plantID: 166,
   },
   myPlants2: {
-    name: 'habanero',
-    plantID: 94
-  }
-}
+    name: "habanero",
+    plantID: 94,
+  },
+  customTask1: {
+    month: "June",
+    week: "2",
+    crop: "potato",
+    task: "smell their delicious flavor",
+    userCreated: true,
+  },
+  customTask2: {
+    month: "July",
+    week: "2",
+    crop: "devil Fruit",
+    task: "be afraid",
+    userCreated: true,
+  },
+};
