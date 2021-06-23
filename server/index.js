cc088804628fff15ed1088c3e74cad1aa4d7c11a
Calimeron <ron.chamma@gmail.com>
2021-06-23 09:53:06 +0200

    :recycle:  put the server logic in an external file for testing purposes

--- server/index.js
+++ server/index.js
@@ -1,17 +1,7 @@
 "use strict";
-
-require("dotenv").config();
-const express = require("express");
-const router = require("./router");
-const cors = require("cors");
-require("./models/database");
-
+const app = require('./server');
 const PORT = process.env.PORT || 3001;
-const app = express();
 
-app.use(cors());
-app.use(express.json());
-app.use(router);
 
 app.listen(PORT, () => {
   console.clear();
---
+++ server/server.js
@@ -0,0 +1,16 @@
+'use strict';
+
+require("dotenv").config();
+const express = require("express");
+const router = require("./router");
+const cors = require("cors");
+require("./models/database");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(router);
+
+
+module.exports = app;
