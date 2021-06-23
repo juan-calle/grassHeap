71067dc2e379ff1e05b794e77836147026751527
Calimeron <ron.chamma@gmail.com>
2021-06-23 12:00:03 +0200

    :white_check_mark:  Add POST /myPlants test + afterAll()
    Add a POST/myPlants test (not tested yet), and an afterAll() to clear the test database after the tests have run

--- server/controllers/__tests__/plantController.test.js
+++ server/controllers/__tests__/plantController.test.js
@@ -1,10 +1,25 @@
 const app = require("../../server");
 const supertest = require("supertest");
 const request = supertest(app);
+const mocks = require('./mocks');
+const db = require('../../models/database');
 
+async function clearMyPlantsDB () {
+  await db.dropCollection('myplants');
+}
 
+afterAll(() => {
+  return clearMyPlantsDB();
+})
 
-
+describe('POST /myPlants', () => {
+  it('should post plant to myplants collection', (done) => {
+    request
+      .post('myPlants')
+      .send(mocks.myPlants1)
+    .expect(201)
+  })
+})
 
 
 describe('GET /myPlants', () => {
