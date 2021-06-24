c3f8fc7be2c6889a3b30673544e833c6c7cffd3e
Calimeron <ron.chamma@gmail.com>
2021-06-24 10:32:19 +0200

    :bug:  Fix weather API service

--- client/src/services/WeatherApiServices.tsx
+++ client/src/services/WeatherApiServices.tsx
@@ -1,14 +1,16 @@
 import { SERVER_URL as base_url } from '../utils/config';
 import { APIWeather } from '../common/types';
-
-export const getWeather = async (city = ''): Promise<APIWeather> => {
+/* eslint-disable */
+export const getWeather = async (city = ""): Promise<APIWeather> => {
+  console.log("in services")
   try {
+    console.log(JSON.stringify(city));
     const JSONweather = await fetch(`${base_url}/weather`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify(city),
+      body: JSON.stringify({ city }),
     });
     const weather = await JSONweather.json();
     if (weather.cod === '400' || weather.cod === '404') throw new Error();
