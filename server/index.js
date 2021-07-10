c06532bf06aefe248283ccc608731aa6d5385819
Gil Reich <gilreich12@gmail.com>
2021-07-10 14:20:04 +0100

    chore: clean up merge commit

--- server/index.js
+++ server/index.js
@@ -1,19 +1,6 @@
 "use strict";
-<<<<<<< HEAD
-
-require("dotenv").config();
-const express = require("express");
-const router = require("./router");
-const cors = require("cors");
-require("./models/database");
-
-const PORT = process.env.PORT;
-const app = express();
-=======
-const app = require('./server');
+const app = require("./server");
 const PORT = process.env.PORT || 3001;
->>>>>>> 4dc1d82d2d454840a4eab416129be622a517510d
-
 
 app.listen(PORT, () => {
   console.clear();
