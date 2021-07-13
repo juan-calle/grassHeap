ad16ebd3c1a4428fdccefa1a5c959f13ec722177
Gil Reich <gilreich12@gmail.com>
2021-07-13 11:17:40 +0100

    fix: change config for running tests

--- server/models/database.js
+++ server/models/database.js
@@ -1,18 +1,21 @@
 const DB_USER = process.env.DB_USER;
 const DB_USER_PASSWORD = process.env.DB_USER_PASSWORD;
 
+const DB_NAME =
+  process.env.NODE_ENV === "test"
+    ? process.env.DATABASE_TEST
+    : process.env.DATABASE;
+
+let connectionURI;
+if (DB_USER && DB_USER_PASSWORD && process.env.NODE_ENV !== "test")
+  connectionURI = `mongodb+srv://${DB_USER}:${DB_USER_PASSWORD}@cluster0.kbmrd.mongodb.net/grassHeap?retryWrites=true&w=majority`;
+else connectionURI = `mongodb://localhost:27017/${DB_NAME}`;
+
 const mongoose = require("mongoose");
-mongoose.connect(
-  `mongodb://localhost:${
-    process.env.NODE_ENV === "test"
-      ? process.env.DATABASE_TEST
-      : process.env.DATABASE
-  }`,
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  }
-);
+mongoose.connect(connectionURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 const db = mongoose.connection;
 
